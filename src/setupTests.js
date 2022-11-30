d7874bc75cfc12219a14d091be8f644bdf61a6ae
ARAVINDAKUMAR S <119474792+renfieldaravind35@users.noreply.github.com>
2022-11-30 14:24:57 +0530

    Add files via upload

---
+++ src/setupTests.js
@@ -0,0 +1,33 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "source": [
+        "// jest-dom adds custom jest matchers for asserting on DOM nodes.\n",
+        "// allows you to do things like:\n",
+        "// expect(element).toHaveTextContent(/react/i)\n",
+        "// learn more: https://github.com/testing-library/jest-dom\n",
+        "import '@testing-library/jest-dom';"
+      ],
+      "metadata": {
+        "id": "0wfZnYTqAe-Q"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
