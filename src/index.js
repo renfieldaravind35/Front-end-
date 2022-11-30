2909d482228b4d3ee3e21b675c397b6cc97ceb2d
ARAVINDAKUMAR S <119474792+renfieldaravind35@users.noreply.github.com>
2022-11-30 14:23:46 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,45 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "source": [
+        "import React from 'react';\n",
+        "import ReactDOM from 'react-dom';\n",
+        "import './index.css';\n",
+        "import App from './App';\n",
+        "// import reportWebVitals from './reportWebVitals';\n",
+        "\n",
+        "ReactDOM.render(\n",
+        "  <React.StrictMode>\n",
+        "    <App />\n",
+        "  </React.StrictMode>,\n",
+        "  document.getElementById('root')\n",
+        ");\n",
+        "\n",
+        "// If you want to start measuring performance in your app, pass a function\n",
+        "// to log results (for example: reportWebVitals(console.log))\n",
+        "// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals\n",
+        "// reportWebVitals();"
+      ],
+      "metadata": {
+        "id": "PI2ukjvCANp-"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
